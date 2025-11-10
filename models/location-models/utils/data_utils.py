366e5def765281ba5e66649118255aa015be50ba
Emmanuel Teitelbaum <emmanuel.teitelbaum@gmail.com>
2025-11-10 16:41:38 -0500

    fix indentation error

--- models/location-models/utils/data_utils.py
+++ models/location-models/utils/data_utils.py
@@ -154,7 +154,7 @@ def tokenize_seq2seq(examples, tokenizer, max_input_length=512, max_target_lengt
     )
     
     # Encode targets using text_target to avoid deprecated as_target_tokenizer()
-        target_encodings = tokenizer(
+    target_encodings = tokenizer(
         text_target=examples['target'],
         max_length=max_target_length, 
         truncation=True, 
